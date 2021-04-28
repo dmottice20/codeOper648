4cd839698d4c3d6c3ffd040273de76e505004968
dmottice20 <davidmottice.bh@gmail.com>
2021-04-28 14:23:10 -0400

    saving work for Problem 2

--- backward_induction.py
+++ backward_induction.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 ################################################
-##  PART 0 - Initialization & Preprocessing   ##
+#  PART 0 - Initialization & Preprocessing     #
 ################################################
 # Decision epochs...
 N = 10
-T = np.arange(N-1)
+T = np.arange(N - 1)
 # Set of nodes...
 M = 8
 S = np.arange(M)
@@ -15,33 +15,33 @@ A = dict()
 cardA = dict()
 
 for s in S:
-    if s == 1:
+    if s == 1-1:
         A[s] = [1, 2, 3]
-    elif s == 2:
+    elif s == 2-1:
         A[s] = [4, 5]
-    elif s == 3:
+    elif s == 3-1:
         A[s] = [4, 5, 6]
-    elif s == 4:
+    elif s == 4-1:
         A[s] = [6]
     else:
         A[s] = [7]
-    
+
     cardA[s] = len(A[s])
 
 # Instantiate some problem data...
-C = np.zeros((M,M))
-C[0,1] = 3
-C[0,2] = 4
-C[0,3] = 2
-C[1,4] = 4
-C[1,5] = 5
-C[2,4] = 5
-C[2,5] = 6
-C[2,6] = 1
-C[3,6] = 2
-C[4,7] = 1
-C[5,7] = 2
-C[6,7] = 6
+C = np.zeros((M, M))
+C[0, 1] = 3
+C[0, 2] = 4
+C[0, 3] = 2
+C[1, 4] = 4
+C[1, 5] = 5
+C[2, 4] = 5
+C[2, 5] = 6
+C[2, 6] = 1
+C[3, 6] = 2
+C[4, 7] = 1
+C[5, 7] = 2
+C[6, 7] = 6
 C = -C
 
 # Probability of moving to selected node...
@@ -51,7 +51,7 @@ q = 0.8
 P = dict()
 r = dict()
 
-for a in range(0,M):
+for a in range(0, M):
     P[a] = np.zeros((cardS, cardS))
     r[a] = np.zeros((cardS, 1))
 
@@ -63,49 +63,48 @@ for s in S:
         for j in S:
             if len(A[s]) == 1:
                 if j == a:
-                    P[a][s,j] = 1
+                    P[a][s, j] = 1
             else:
                 if j == a:
-                    P[a][s,j] = q
+                    P[a][s, j] = q
                 elif j in A[s]:
-                    P[a][s,j] = (1-q) / (cardA[s]-1)
-        
+                    P[a][s, j] = (1 - q) / (cardA[s] - 1)
+
         r[a][s] = np.matmul(P[a][s][:], C[s][:].transpose())
 
 for p in P.keys():
     print(f"On index {p}, the matrix is\n\n{P[p]}")
     print(P[p].shape)
 
-# Initalize decision rules
+# Initialize decision rules
 d_star = dict()
 for t in T:
     d_star[t] = np.zeros((cardS, 1))
 
-
 ################################################
-##  PART 1 - Running of Backward Induction    ##
+#  PART 1 - Running of Backward Induction      #
 ################################################
 
 # Step 1
 u_star = dict()
-t = N-1
+t = N - 1
 u_star[t] = np.zeros((cardS, 1))
 
 # Steps 2-4
 while t > 0:
-    t-=1
+    t -= 1
     print(f"on t={t}...")
     u_star[t] = np.zeros((cardS, 1))
     for s in S:
         u_t_best = -1000000
         a_star = None
         for a in A[s]:
-            u_t = r[a][s] + np.matmul(P[a][s][:], u_star[t+1])
+            u_t = r[a][s] + np.matmul(P[a][s][:], u_star[t + 1])
             if u_t > u_t_best:
                 u_t_best = u_t
                 a_star = a
 
-        u_star[t][s] = r[a_star][s] + np.matmul(P[a_star][s][:], u_star[t+1])
+        u_star[t][s] = r[a_star][s] + np.matmul(P[a_star][s][:], u_star[t + 1])
         d_star[t][s] = a_star
 
 print("\n\n========POLICY VALUES========\n\n")
--- hw3/problem2.py
+++ hw3/problem2.py
@@ -1,14 +1,20 @@
 import numpy as np
 import logging
 
-###############################################
-##       Initialize & Preprocessing          ##
-###############################################
-# Decision epochs
-T = 21
+logging.basicConfig(filename='problem2.log', filemode='w',
+                    format='%(asctime)s | %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
 
+###############################################
+#       Initialize & Preprocessing            #
+###############################################
+logging.info("=========INPUT=========")
+# Decision epochs
+N = 21
+T = np.arange(N + 1)
 # Construct state space...
-S = np.arange(4,10+1)
+S = np.arange(4, 10 + 1)
 # add a state = 11 that represents the delta 
 # we are keeping as numbers so that we don't have to switch to
 # a range() (more memory and slower) as well as use a character.
@@ -21,17 +27,17 @@ card_a = dict()
 
 for s in S:
     if s < 11:
-        A[s] = np.asarray([1,2,3])
+        A[s] = np.asarray([1, 2, 3])
     else:
         A[s] = np.asarray([0])
-    
+
     card_a[s] = len(A[s])
 
 # Construct transition matrices and reward vectors...
 P = dict()
 r = dict()
 
-for a in np.arange(0,3+1):
+for a in np.arange(0, 3 + 1):
     P[a] = np.zeros((card_s, card_s))
     r[a] = np.zeros((card_s, 1))
 
@@ -66,29 +72,66 @@ for s in S:
     for a in A[s]:
         for j in S:
             if s == 11:
-                if a == 0:
-                    if j == 11:
-                        P[a][s-indx_offset, j-indx_offset] = 1
-            if s in np.arange(4, 4+h):
+                if j == 11:
+                    P[a][s - indx_offset, j - indx_offset] = 1
+            if s in np.arange(4, 4 + h):
                 # Check if j = delta
                 if j == 11:
-                    P[a][s-indx_offset,j-indx_offset] = q[a]+(1-q[a])*(1-f[a])
-            if s in np.arange(4+h,10+1):
+                    P[a][s - indx_offset, j - indx_offset] = q[a] + (1 - q[a]) * (1 - f[a])
+            if s in np.arange(4 + h, 10 + 1):
                 # Check if j = s-1
-                if j == s-1:
-                    P[a][s-indx_offset,j-indx_offset] = (1-q[a])*(1-f[a])
+                if j == s - 1:
+                    P[a][s - indx_offset, j - indx_offset] = (1 - q[a]) * (1 - f[a])
                 if j == 11:
-                    P[a][s-indx_offset,j-indx_offset] = q[a]
+                    P[a][s - indx_offset, j - indx_offset] = q[a]
             if s in S[:-1]:
                 # Check if j = min(s+g-h,10)
-                if j == min(s+g[a]-h, 10):
-                    P[a][s-indx_offset,j-indx_offset] = (1-q[a])*f[a]
-        
-        r[a][s-indx_offset] = 0
+                if j == min(s + g[a] - h, 10):
+                    P[a][s - indx_offset, j - indx_offset] = (1 - q[a]) * f[a]
 
-for a in P:
-    print(f"\nFor a={a}, P[{a}] is...\n",P[a])
+        r[a][s - indx_offset] = 0
+
+logging.info("The transition matrices are built.")
+for a in P.keys():
+    logging.info(f"For a={a}, P is...\n{P[a]}")
 
 ###############################################
-##       Backward Induction Solving          ##
+#       Backward Induction Solving            #
 ###############################################
+u_star = dict()
+t = N
+
+# Initliaze a decsion rule struct
+d_star = dict()
+for t in T:
+    d_star[t] = np.zeros((card_s, 1))
+
+# Initialize U_star[n] = r_N(X_n)
+u_star[N] = np.vstack((np.ones((card_s - 1, 1)), np.asarray([0])))
+
+while t > 0:
+    t -= 1
+    u_star[t] = np.zeros((card_s, 1))
+    # For s in S, compute u_t(s)
+    for s in S:
+        u_t_best = -10000
+        a_star = None
+        for a in A[s]:
+            # Compute u*t_(s) when applying action a
+            # 1) calculate expected immediate reward
+            # 2) expected total reward when executing
+            #    policy pi* during subsequent periods
+            u_t = r[a][s - indx_offset] + np.matmul(P[a][s - indx_offset, :], u_star[t + 1])
+            if u_t > u_t_best:
+                u_t_best = u_t
+                a_star = a
+
+        u_star[t][s - indx_offset] = r[a_star][s - indx_offset] + np.matmul(P[a_star][s - indx_offset, :],
+                                                                            u_star[t + 1])
+        d_star[t][s - indx_offset] = a_star
+
+logging.info("========OUTPUT========")
+logging.info("---POLICY VALUES---")
+logging.info(f"The u_star is ...\n{u_star}")
+logging.info("---Policy Decision---")
+logging.info(f"The d_star is ...\n{d_star}")
